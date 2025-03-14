084a3285d15892efca5125477af571a8b44b4813
PavelStreit70 <streit.pavel@email.cz>
2025-03-14 09:35:45 +0100

    ukol s tucnaky

--- data_penguins.R
+++ data_penguins.R
@@ -6,3 +6,16 @@ data <- penguins
 view(data)
 
 str(data)
+
+
+#Using the scatterplot, plot the dependency of body mass on the flipper length
+#Add a graph title
+#Change the color based on the penguin species
+#Change the points type based on island they live on
+
+plot(data$flipper_length_mm, y = data$body_mass_g,
+     main = "Palmer Penguins: body mass based on flipper length",
+     colors <- rainbow(data$species), pch = c(15, 16, 17)[as.integer(data$island)]))
+  
+
+
